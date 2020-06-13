23fbeb8bd3e5af491d197176f011011b47140f5b
laisrhianne <lays.rhianne@gmail.com>
2020-06-12 21:13:25 -0300

    m1e5 added

---
+++ es6/exercicios/m4e5.js
@@ -0,0 +1,25 @@
+// 5.1 Rest
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+const [x, ...y] = arr;
+console.log(x);
+console.log(y);
+
+function soma(...params) {
+  return params.reduce((total, next) => total + next);
+}
+
+console.log(soma(5, 6, 4));
+
+// 5.2 Spread
+
+const usuario = {
+  nome: 'Diego',
+  idade: 23,
+  endereco: { cidade: 'Rio do Sul', uf: 'SC', pais: 'Brasil' },
+};
+
+const usuario2 = {...usuario, nome: 'Gabriel'};
+const usuario3 = {...usuario, endereco: {...usuario.endereco, cidade: 'Lontras'}}
+console.log(usuario3);
